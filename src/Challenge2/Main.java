a5183eee1e725ebaf1f92b6a95aa4364c464fc82
KT147 <kevin.tammm@gmail.com>
2026-05-22 22:00:59 +0300

    Student streaming

--- src/Challenge2/Main.java
+++ src/Challenge2/Main.java
@@ -1,5 +1,9 @@
 package Challenge2;
 
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -13,9 +17,60 @@ public class Main {
 //        System.out.println(tim);
 
 
-        Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
-                .limit(10)
-                .forEach(System.out::println);
+        List<Student> students = Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+                .limit(1000)
+                .toList();
+
+        long maleStudents = students.stream()
+                .filter(student -> student.getGender().equalsIgnoreCase("M"))
+                .count();
+
+        long femaleStudents = students.stream()
+                .filter(student -> student.getGender().equalsIgnoreCase("F"))
+                .count();
+
+        long lessThan30 = students.stream()
+                .filter(student -> student.getAge() < 30)
+                .count();
+
+        long between30and60 = students.stream()
+                .filter(student -> student.getAge() > 30 && student.getAge() < 60)
+                .count();
+
+        long over60 = students.stream()
+                .filter(student -> student.getAge() < 60)
+                .count();
+
+        IntSummaryStatistics stats = students.stream()
+                .collect(Collectors.summarizingInt(Student::getAge));
+
+        List<String> studentCountries = students.stream()
+                .map(Student::getCountryCode)
+                //ei korda samu
+                .distinct()
+                .toList();
+
+        boolean isActive = students.stream()
+                .anyMatch(student ->
+                        student.getMonthsSinceActive() == 0 &&
+                                student.getYearsSinceEnrolled() >= 7
+                );
+
+        List<Student> fiveStudents = students.stream()
+                        .limit(5)
+                                .toList();
+
+
+        System.out.println("There are " + maleStudents + " male students");
+        System.out.println("There are " + femaleStudents + " female students");
+        System.out.println("There are " + lessThan30 + " students less than 30 years old");
+        System.out.println("There are " + between30and60 + " students between 30 and 60");
+        System.out.println("There are " + over60 + " students over 60");
+        System.out.println("Average student age is " + (int) stats.getAverage());
+        System.out.println("Students are from: " + studentCountries);
+        System.out.println("isActive= " + isActive);
+        fiveStudents.forEach(System.out::println);
+
 
     }
 }
